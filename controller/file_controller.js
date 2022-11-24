62c7bd0cb8901e672b4b276034b4944c04e2cede
mayur mali <mayurmali31@gmail.com>
2022-11-24 14:35:59 +0530

    final setup

---
+++ controller/file_controller.js
@@ -0,0 +1,29 @@
+const csv = require("csv-parser");
+const fs = require("fs");
+const path = require("path");
+const File = require("../models/files");
+
+/**********EXPORTING FUNCTION FOR View ROUTE******************/
+module.exports.View = async function (req, res) {
+  let filePATH = await File.findOne({ file: req.params.file });
+
+  const results = [];
+  const header = [];
+  fs.createReadStream(filePATH.filePath)
+    .pipe(csv())
+    .on("headers", (headers) => {
+      headers.map((head) => {
+        header.push(head);
+      });
+      console.log(header);
+    })
+    .on("data", (data) => results.push(data))
+    .on("end", () => {
+      res.render("file", {
+        title: filePATH.originalName,
+        head: header,
+        data: results,
+        length: results.length,
+      });
+    });
+};
---
+++ controller/upload_controller.js
@@ -0,0 +1,43 @@
+/****************IMPORTING PACKAGE/MODELS*************************/
+const File = require("../models/files");
+
+/**********EXPORTING FUNCTION FOR Uploading ROUTE******************/
+module.exports.Upload = function (req, res) {
+  try {
+    //Use for uploading file with note
+    File.uploadedFile(req, res, function (err) {
+      if (err) {
+        console.log("multer Error", err);
+      }
+
+      if (
+        (req.file && req.file.mimetype == "application/vnd.ms-excel") ||
+        (req.file && req.file.mimetype == "text/csv")
+      ) {
+        File.create(
+          {
+            filePath: req.file.path,
+            originalName: req.file.originalname,
+            file: req.file.filename,
+          },
+          function (err) {
+            if (err) {
+              console.log("error", err);
+              return res.status(400).json({
+                message: "Error in creating Note or Uploading File",
+              });
+            }
+
+            return res.redirect("/");
+          }
+        );
+      } else {
+        console.log("Please Upload CSV Format file");
+
+        return res.redirect("/");
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
