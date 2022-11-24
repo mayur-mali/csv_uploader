d628cf154bb38631f1df3b9025386abbf999c2ee
mayur-mali <mayurmali31@gmail.com>
2022-11-24 21:53:12 +0530

    minor changes

--- models/files.js
+++ models/files.js
@@ -2,38 +2,39 @@
 const mongoose = require("mongoose");
 const multer = require("multer");
 const path = require("path");
-const FILES_PATH = path.join("/uploads/files");
-
+const FILES_PATH = path.join("/upload/files");
+console.log(FILES_PATH);
 /***************CREATING USER SCHEMA*****************************/
-const fileSchema = new mongoose.Schema({
-  filePath:{
-    type: String
+const fileSchema = new mongoose.Schema(
+  {
+    filePath: {
+      type: String,
+    },
+    originalName: {
+      type: String,
+    },
+    file: {
+      type: String,
+    },
   },
-  originalName:{
-    type: String
-  } , 
-  file:{
-        type:String
-    }
-},{
-    timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 /*******SETTINGS FOR UPLOADING FILE USING MULTER****************/
 let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, "..", FILES_PATH));//exact path where files need to uplaod
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now());
-    }
-  });
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "..", FILES_PATH)); //exact path where files need to uplaod
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
 
-
-  //static functions
-  fileSchema.statics.uploadedFile = multer({ storage: storage }).single("file");
-  fileSchema.statics.filePath = FILES_PATH;//Making FILES_PATH publicly available
-
+//static functions
+fileSchema.statics.uploadedFile = multer({ storage: storage }).single("file");
+fileSchema.statics.filePath = FILES_PATH; //Making FILES_PATH publicly available
 
 /******************MAKING MODEL*********************************/
 const Files = mongoose.model("Files", fileSchema);
